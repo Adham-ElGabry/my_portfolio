fa5efbde1f8a5d8175c3b5ad7c2ff962feddf115
Adham El-Gabery <adhamelgabery@gmail.com>
2025-02-11 21:22:52 +0200

    my_portfolio
    my personal website is hereee

---
+++ theme.config.js
@@ -0,0 +1,10 @@
+const YEAR = new Date().getFullYear()
+
+export default {
+  footer: (
+    <small style={{ display: 'block', marginTop: '8rem' }}>
+      <time>{YEAR}</time> © Adham El-Gabry.
+
+    </small>
+  )
+}
